0e27db5393d8a005abe245de0a76f612a19c7023
Mrgsq <1634508216@qq.com>
2019-10-06 12:14:04 +0800

    222

--- myapp/src/pages/index.vue
+++ myapp/src/pages/index.vue
@@ -2,7 +2,7 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-10-03 20:18:43
- * @LastEditTime: 2019-10-03 22:10:42
+ * @LastEditTime: 2019-10-06 01:06:13
  * @LastEditors: Please set LastEditors
  -->
 <template>
@@ -19,40 +19,76 @@
     </header>
     <div id="container">
       <div class="block">
-        <span class="demonstration">默认 Hover 指示器触发</span>
+        <!-- <span class="demonstration">默认 Hover 指示器触发</span> -->
         <el-carousel height="150px">
-          <el-carousel-item v-for="item in 4" :key="item">
-            <h3 class="small">{{ item }}</h3>
+          <el-carousel-item v-for="item in lunbotuImg" :key="item">
+            <!-- <h3 class="small">{{ item }}</h3> -->
+            <!-- <img src="../assets/img/lb1.png" alt /> -->
+            <img :src="item" alt style="height:100%;width:100%;" />
           </el-carousel-item>
         </el-carousel>
       </div>
-      <el-row :gutter="20">
-        <el-col :span="6">
+      <el-row :gutter="20" style="width:100%; marign-left:0" >
+        <el-col :span="6" v-for='(item) in titlebox' :key='item.title'>
           <div class="grid-content bg-purple">
-            <img src alt />
-            <p>社保服务</p>
+            <img :src="item.imgurl" alt  style='width:2.25rem;height:2.25rem;'/>
+            <p>{{item.title}}</p>
           </div>
         </el-col>
-        <el-col :span="6">
-          <div class="grid-content bg-purple">
-            <img src alt />
-            <p>公积金</p>
-          </div>
-        </el-col>
-        <el-col :span="6">
-          <div class="grid-content bg-purple">
-            <img src alt />
-            <p>企业服务</p>
-          </div>
-        </el-col>
-        <el-col :span="6">
-          <div class="grid-content bg-purple">
-            <img src alt />
-            <p>增值服务</p>
-          </div>
-        </el-col>
+        
       </el-row>
-      <div id="footer"></div>
+      <div class="shiftTips">
+        <div class="laba">
+          <img src="../assets/img/new1.png" alt="">
+        </div>
+        <ul class="tips">
+          <transition>
+          <li class="tip tip1" v-bind="move()" :style='transform'><a href="###">关于最新城乡医保的分析通知</a></li>
+          </transition>
+          <transition>
+          <li class="tip tip2" v-bind='move()' ><a href="###" >什么是五险一金</a></li>
+          </transition>
+        </ul>
+      </div>
+      <div class="colee"></div>
+      
+      <div class="serverList">
+        <div class="server_header list_title">
+          <span>服务套餐</span>
+          <span>Discount package</span>
+        </div>
+        <div class="meals-index">
+          <div class="meal-index">
+            <div class="meallist" >
+              <div class="mealitem" style="width:10rem;" v-for='item in shebaodata' :key='item.title'>
+                <div class="mealtitle">{{item.name}}</div>
+                <div class="mealmonth">参保月数{{item.start}}-{{item.end}}个月</div>
+                <div class="discount">优惠价：{{item.price}}元/月</div>
+                <div class="costprice">原价：{{item.original_price}}元/月</div>
+                <div class="canbaoSubmit">
+                  <img src="../assets/img/canbao1.png" alt="">
+                </div>
+              </div>
+              
+            </div>
+            
+          </div>
+          <div class="rightbtn">
+            <i class="el-icon-arrow-right"></i>
+          </div>
+          <p class="ps-index">*社保公积金同时缴纳，只收取一项服务费</p>
+
+        </div>
+       
+      </div>
+       <div class="colee"></div>
+       <div class="cations">
+         <div class="cations_header list_title">
+           <span>资质展示</span>
+           <span>Qualification show</span>
+         </div>
+         <div class="cationsBanner"></div>
+       </div>
     </div>
     <footer>
       <ul id="foot-btns">
@@ -79,20 +115,81 @@
 <script>
 import Vue from "vue";
 import ElementUI from "element-ui";
+import axios from 'axios';
 import "element-ui/lib/theme-chalk/index.css";
-// import App from './App.vue';
+// import $ from 'jquery';
+// // import App from './App.vue';
+// console.log($)
+//   $('.tips .tip').css('top','2.25rem');
+//   let num = 0;
+//   let timer = setInterval(()=>{
+//     num ++;
+//     if(num%2==0){
+//       $('.tips tip')
+//     }
+//   },1000)
+
 
 Vue.use(ElementUI);
+
+let lb = [  require("../assets/img/lb1.png"),
+        require("../assets/img/lb2.png"),
+        require("../assets/img/lb3.png")
+        ]; 
+        var tip = document.querySelector('.tip1');
 import "bootstrap/dist/css/bootstrap.css";
+
 export default {
   data() {
     return {
-      lunbotuImg: []
+      shiftNum:2,
+      transform:{transform:'translateY(0rem)'},
+      lunbotuImg: lb,
+      shebaodata:'',
+      titlebox: [
+        {
+          imgurl: require("../assets/img/bao1.png"),
+          title: "社保服务"
+        },
+        { imgurl: require("../assets/img/fund1.png"), title: "公积金" },
+        { imgurl: require("../assets/img/qi1.png"), title: "企业服务" },
+        { imgurl: require("../assets/img/zeng1.png"), title: "增值服务" }
+      ]
     };
+  },
+  methods:{
+    shiftTitle:function () {
+      this.style.top = -20 + 'rem';
+    },
+    move:function(){
+     
+     
+      // this.transform = {transform:'translateY(-2rem)'};
+      setInterval(()=>{
+        this.shiftNum -= 2;
+    //  tip.style.transform =`translateY(-${this.shiftNum}rem)`
+      },500)
+    },
+    getData(){
+      var api = 'http://www.jzbshebao.cn/app/common/getServiceCharge';
+      axios.get(api).then(res=>{
+        // let data = res.data.data;
+        
+        this.shebaodata = res.data.data.reverse();
+        console.log(this.shebaodata)
+      })
+    }
+  },
+  created(){
+    this.getData();
   }
 };
 </script>
 <style lang="scss" scoped>
+*{
+  margin: 0;padding: 0;
+  box-sizing: border-box;
+}
 ul,
 li {
   list-style: none;
@@ -141,16 +238,185 @@ li {
     position: absolute;
     top: 2.2rem;
     .el-row {
+      margin-top: 0.8rem;
       .el-col {
         .grid-content {
           display: flex;
           flex-direction: column;
           align-items: center;
+          p{
+            line-height: 2rem;
+            font-size: 0.9rem;
+            
+          }
         }
       }
     }
-    #footer {
-      height: 2.7rem;
+    .shiftTips::before{
+      content: '';
+      border-top: 0.01rem solid #ccc;
+    }
+    .shiftTips{
+      
+      box-sizing: border-box;
+      padding: 0 0.7rem;
+      height:2.25rem;
+      padding: 0 0.7rem;
+      display: flex;
+      align-items: center;
+      .laba{
+        height: 100%;
+        line-height: 2.25rem;
+        img{
+          width: 1.05rem;
+          height: 0.95rem;
+        }
+      }
+      .tips{
+        box-sizing: border-box;
+        padding-left: 0.7rem;
+        margin: 0;
+        height: 100%;
+        // overflow: hidden;
+        position: relative;
+        width: calc(100% - 0.85rem);
+        .tip{
+          transition: all 0.5s;
+          height:100%;
+          line-height: 2.25rem;
+          position: absolute;
+          a{ font-size: 0.8rem;
+            color: #444;
+          }
+        }
+      }
+    }
+    .colee {
+     height: 0.25rem;
+     background: #eee;
+     margin-top: 0.2rem;
+    }
+    .list_title{
+        padding-left: 0.4rem;
+        box-sizing: border-box;
+        span:nth-child(1){
+          font-size: 1rem;
+          color: #222;
+          margin-right: 0.5rem;
+        }
+        span:nth-child(2){
+          font-size: 0.45rem;
+          color: #909090;
+        }
+      } 
+     
+    .serverList{
+      position: relative;
+      padding: 1rem 0.7rem 0.75rem 0.7rem;
+      
+      .list_title::before{
+        position: absolute;
+        // top: 0;
+        left: 0.6rem;
+        content: '';
+        width: 0.2rem;
+        height: 1.3rem;
+        background: #e88346;
+      }
+      .meals-index{
+        
+        .meal-index{
+          width: 100%;
+          position: relative;
+          margin-top: 0.75rem;
+          overflow: auto;
+          .meallist{
+            display: flex;
+            width: 43.1rem;
+            .mealitem{
+              width: 9rem;
+              background: rgba(251,142,77,.1);
+              padding:0.75rem 0.7rem;
+              margin-right: 0.7rem;
+              text-align: center;
+              border-radius: 0.5rem;
+              .mealtitle{
+                font-size: 0.7rem;
+                font-weight: 500;
+                color:#333;
+                padding-bottom: 0.7rem;
+              }
+              .mealmonth{
+                font-size: 0.55rem;
+                color: #666;
+                padding-bottom: 0.7rem;
+              }
+              .discount{
+                font-size:0.7rem;
+                color:#ef8631;
+                padding-bottom: 0.7rem;
+
+              }
+              .costprice{
+                font-size: 0.55rem;
+                color: #666;
+                padding-bottom: 0.7rem;
+              }
+              .canbaoSubmit{
+                img{
+                  width: 4.25rem;
+                  height: 1.3rem;
+                }
+                
+              }
+            }
+          }
+         
+        }
+     .rightbtn{
+            position: absolute;
+            width: 1.6rem;
+            height: 1.6rem;
+            background: #fff;
+            border-radius: 50%;
+            box-shadow: 0 2px 10px 0 rgba(59,60,60,0.2);
+            opacity: 0.75;
+            right: 0.7rem;
+            top: 50%;
+            text-align: center;
+            line-height: 1.6rem;
+            transform: translateY(-50%);
+          i{
+                
+            color: #e88346;
+            font-size: 1rem;
+            font-weight: 700;
+          }
+          }
+        .ps-index{
+          color: #f20600;
+          font-size: 0.7rem;
+          padding-top: 0.75rem;
+        }
+      }
+    }
+    .cations{
+      position: relative;
+      
+      padding: 0 0.7rem;
+      padding-top: 1rem;
+       .list_title::before{
+        position: absolute;
+        // top: 0;
+        left: 0.6rem;
+        content: '';
+        width: 0.2rem;
+        height: 1.3rem;
+        background: #e88346;
+      }
+      .cationsBanner{
+        padding: 1.25rem 0;
+      }
     }
   }
   footer {
@@ -169,9 +435,9 @@ li {
         height: 100%;
         text-align: center;
         color: #fff;
-       .foot-font{
-         font-size: 0.4rem;
-       }
+        .foot-font {
+          font-size: 0.4rem;
+        }
       }
     }
   }
